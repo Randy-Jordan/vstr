5e2dd3e0f2357c7be56fe75fc2398c70d79f0a8b
Randy Jordan <randyjordan@email.com>
2024-08-04 19:11:19 -0500

    Finished interface

--- include/vstr.h
+++ include/vstr.h
@@ -16,3 +16,14 @@ typedef struct Vstr Vstr;
 #define VSTR(cstr) vstr_n(cstr, sizeof(cstr) - 1)
 #define VSTR_NULL vstr_n(NULL, 0)
 
+Vstr vstr_s(const char *s);
+Vstr vstr_n(const char *s, size_t n);
+bool vstr_cmp(const Vstr v1, const Vstr v2);
+const char *vstr_str(const Vstr haystack, const Vstr needle);
+Vstr vstr_chop(Vstr v, const char *delims);
+Vstr vstr_substr(Vstr v, size_t start, size_t stop);
+Vstr vstr_trim(Vstr v);
+Vstr vstr_trim_left(Vstr v);
+Vstr vstr_trim_right(Vstr v);
+
+#endif
